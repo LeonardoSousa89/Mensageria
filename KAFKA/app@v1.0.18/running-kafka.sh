2dd65d9e9fab94d14c1df40d4e130b9d1e86f7ca
Mendes Sousa <leoprofessionallogytech@outlook.com>
2023-01-15 17:23:26 -0300

    @v1.0.18

---
+++ KAFKA/app@v1.0.18/running-kafka.sh
@@ -0,0 +1,15 @@
+# copie e cole no terminal
+
+git clone https://github.com/LeonardoSousa89/Mensageria.git  &&
+cd Mensageria &&
+cd KAFKA &&
+cd mensageria &&
+sudo docker-compose up
+
+
+# copie e cole no terminal de execução do arquivo de criação do tópico de teste
+cd Mensageria &&
+cd KAFKA &&
+cd mensageria &&
+chmod a+x topic-test.sh &&
+./topic-test.sh
