5c48925e8ad9aa368a919e2cc828c9dde3598a4a
Mendes Sousa <leoprofessionallogytech@outlook.com>
2023-01-15 23:16:43 -0300

    @v1.0.32

--- KAFKA/app@v1.0.31/running-kafka.sh
+++ KAFKA/app@v1.0.31/running-kafka.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/LeonardoSousa89/Mensageria.git  &&
 cd Mensageria &&
 cd KAFKA &&
-cd 'app@v1.0.22' &&
+cd 'app@v1.0.31' &&
 cd consumer &&
 sudo docker build -t leozin89/consumer:v1 . &&
 sudo docker login -u leozin89 &&
@@ -19,10 +19,9 @@ sudo docker-compose up
 # copie e cole no terminal de execução do arquivo de criação do tópico de teste
 cd Mensageria &&
 cd KAFKA &&
-cd 'app@v1.0.22' &&
+cd 'app@v1.0.31' &&
 chmod a+x topic-dados.sh &&
 ./topic-dados.sh 
-
-
-sudo docker run -d -p 8767:8767 --name consumer leozin89/consumer:v1 &&
-sudo docker run -d -p 8766:8766 --name producer leozin89/producer:v1 
+ 
+sudo docker run -p 8767:8767 --name consumer --network mensageria leozin89/consumer:v1 &&
+sudo docker run -p 8766:8766 --name producer --network mensageria leozin89/producer:v1 
